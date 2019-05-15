62bccac1aa3209bb433c26ce0d9bae0ee81efdd3
Hoxtygen <hoxtygen@live.com>
2019-05-15 17:59:49 +0100

    Add click event to closeBtn
    The close button main function is to close an article when clicked, it requires an event listener to achieve this, and this was added in this changes.

--- Article/Article.js
+++ Article/Article.js
@@ -2,17 +2,24 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    const expandButton  = domElement.querySelector(".expandButton")
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    expandButton.textContent = "expand";
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    expandButton.addEventListener('click', this.expandArticle)
-    
+        // assign this.domElement to the passed in domElement
+        this.domElement = domElement;
+        // create a reference to the ".expandButton" class. 
+        this.expandButton  = domElement.querySelector(".expandButton")
+        // Using your expandButton reference, update the text on your expandButton to say "expand"
+        this.expandButton.textContent = "expand";
+        // Set a click handler on the expandButton reference, calling the expandArticle method.
+        this.expandButton.addEventListener('click', this.expandArticle);
+        this.createCloseBtn()
   }
+
+    createCloseBtn()  {
+        const closeBtn = document.createElement("button");
+        closeBtn.textContent = "close";
+        closeBtn.classList.add("closeBtn")
+        this.domElement.append(closeBtn) 
+        closeBtn.addEventListener("click", () => this.domElement.style.display = "none")
+    }
 
   expandArticle = () => {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
